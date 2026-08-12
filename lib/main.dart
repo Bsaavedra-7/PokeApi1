9a716cbb5c7b0c47acb37058b435e0a362ebc307
Bsaavedra <Bsaavedra2024@inf.udec.cl>
2026-08-12 16:53:11 -0400

    feat: add pokemon search input and PokeApi connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,68 @@
 import 'package:flutter/material.dart';
+import 'services/pokemon_api.dart';
+
 void main() {
   runApp(const MyApp());
 }
-class MyApp extends StatelessWidget {
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  // AQUÍ va el controller
+  final TextEditingController controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.red,
+        ),
       ),
+
       home: Scaffold(
-        
+
         appBar: AppBar(
           backgroundColor: Colors.red,
           centerTitle: true,
           toolbarHeight: 80,
+
           title: const Text(
             'Pokedex',
             style: TextStyle(
-                fontFamily: 'comic-sans',
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-
+              fontFamily: 'comic-sans',
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
             ),
-          )
+          ),
         ),
-        body: const Center(
-          child: Text('Hola Mundo'),
+
+        body: Column(
+          children: [
+
+            TextField(
+              controller: controller,
+
+              decoration: InputDecoration(
+                hintText: 'Escribe un Pokémon',
+                prefixIcon: const Icon(Icons.search),
+
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+            ),
+
+          ],
         ),
       ),
     );
