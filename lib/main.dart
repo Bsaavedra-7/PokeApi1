000e06d4d133ba8a3bb2f502e41359723e76f948
Bsaavedra <Bsaavedra2024@inf.udec.cl>
2026-08-12 21:59:05 -0400

    feat: first funcional setup

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,8 @@ class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<MyApp> createState() => _MyAppState(); //state como en react
+
 }
 
 class _MyAppState extends State<MyApp> {
@@ -17,10 +18,13 @@ class _MyAppState extends State<MyApp> {
   // AQUÍ va el controller
   final TextEditingController controller = TextEditingController();
 
+  //aca creo otro estado que guardara los datos del pokemon en un array de tipo map
+  Map<String, dynamic>? pokemon;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pokedex',
 
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
@@ -46,26 +50,63 @@ class _MyAppState extends State<MyApp> {
           ),
         ),
 
-        body: Center(
-          child: SizedBox(
-            width: 300,
-            child: TextField(
-              controller: controller,//aca creo el objeto controller para poder obtener el valor del textfield
+        body: Column(
+          children: [
 
+            // INPUT
+            Center(
+              child: SizedBox(
+                width: 300,
+                child: TextField(
+                  controller: controller,
 
-              //funcion igual a la de react que me permite obtener el valor del textfield en tiempo real y comprobar que funciona correctamente
-               onChanged: (value) {
-                print(value);
-              },
-              decoration: InputDecoration(
-                hintText: 'Escribe un Pokémon',
-                prefixIcon: const Icon(Icons.search),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15),
+                  onChanged: (value) async {
+                    if (value.isNotEmpty) {
+                      final data = await obtenerPokemon(value);
+
+                      setState(() {
+                        pokemon = data;
+                      });
+                    }
+                  },
+
+                  decoration: InputDecoration(
+                    hintText: 'Escribe un Pokémon',
+                    prefixIcon: const Icon(Icons.search),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
                 ),
               ),
             ),
-          ),
+
+            // RESULTADO
+            if (pokemon != null)
+              Column(
+                children: [
+                  Text(
+                    pokemon!["name"],
+                    style: const TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  Text(
+                    "Altura: ${pokemon!["height"]}",
+                  ),
+
+                  Text(
+                    "Peso: ${pokemon!["weight"]}",
+                  ),
+
+                  Image.network(
+                    pokemon!["sprites"]["front_default"],
+                  ),
+                ],
+              ),
+          ],
         ),
       ),
     );
--- lib/services/pokemon_api.dart
+++ lib/services/pokemon_api.dart
@@ -1,11 +1,17 @@
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-Future<void>  obtenerPokemon(String nombre) async{
+Future<Map<String, dynamic>>  obtenerPokemon(String nombre) async{
 
     final url = Uri.parse('https://pokeapi.co/api/v2/pokemon/$nombre');
 
     final response = await http.get(url);
 
-    print(response.body);
+    if (response.statusCode == 200){
+      final data = jsonDecode(response.body);
 
+      return data;
+    }else{
+        throw Exception('Error al obtener el Pokémon');
+    }
 }
